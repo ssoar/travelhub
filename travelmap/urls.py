124390deb241b6d180a431c0402c7f530091262e
ssoar <sstdsc717@gmail.com>
2021-03-21 19:59:18 +0900

    commit for admin

--- travelmap/urls.py
+++ travelmap/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/travelhub/')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
