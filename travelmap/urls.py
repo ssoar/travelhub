35a85001d3174e22f528815756b816ce25c6d6a5
ssoar <sstdsc717@gmail.com>
2021-03-22 15:39:26 +0900

    test change

--- travelmap/urls.py
+++ travelmap/urls.py
@@ -9,4 +9,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/travelhub/')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
