b5e49dcb78e46dec07a2726831a892ce902cb756
ssoar <sstdsc717@gmail.com>
2021-03-27 10:46:25 +0900

    try nginx

--- travelmap/settings.py
+++ travelmap/settings.py
@@ -125,13 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = '/home/centos/sample/static/'
+STATIC_ROOT = '/usr/share/nginx/html/static'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-MEDIA_ROOT = '/home/centos/sample/media'
+MEDIA_ROOT = '/usr/share/nginx/html/media'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
